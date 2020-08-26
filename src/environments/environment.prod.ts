79bf3bf78ab0d53990f6aaef9cfa76b20900a752
Siddharth Ganesh <cdart.dav@gmail.com>
2020-08-26 11:43:38 +0530

    fix(env): adds firebase env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA5WkUYT4oJKfcKeuaZ3xAc9EigwBBR2Fc',
+    authDomain: 'gchat-project-6a119.firebaseapp.com',
+    databaseURL: 'https://gchat-project-6a119.firebaseio.com',
+    projectId: 'gchat-project-6a119',
+    storageBucket: 'gchat-project-6a119.appspot.com',
+    messagingSenderId: '171697051876',
+    appId: '1:171697051876:web:462ea814b69c258dbdb4f1'
+  }
 };
